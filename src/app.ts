c68b4b76b9e1aab906541a491179cba7d111d781
Petr Pliska <petr.pliska@post.cz>
2018-02-19 20:59:10 +0100

    Remove connection options and modify glob pattern

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,10 @@
 import * as express from "express";
 import "reflect-metadata";
 import {useExpressServer} from "routing-controllers";
-import {createConnection, ConnectionOptions} from 'typeorm';
-
-// Database connection options
-const connectionOptions: ConnectionOptions = {
-    type: "postgres",
-    url: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_SSL_MODE !== 'disabled'
-};
+import {createConnection} from 'typeorm';
 
 // Connect to database
-createConnection(connectionOptions).then(connection => {
+createConnection().then(async connection => {
     const app: express.Application = express();
     const port: number = Number(process.env.PORT) || 3000;
 
@@ -20,7 +13,7 @@ createConnection(connectionOptions).then(connection => {
             origin: [`http://localhost:${port}`]
         },
         // routePrefix: "/api", //
-        controllers: [__dirname + "/controller/**/*"]
+        controllers: [__dirname + "/controller/**/*.+(js|ts)"]
     });
 
     app.listen(port, () => {
